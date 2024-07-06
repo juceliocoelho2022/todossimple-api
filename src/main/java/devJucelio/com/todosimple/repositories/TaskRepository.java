a0a7861e9a50aefc969adec5591e654937ca4afe
JUCELIO <juceliocoelho2022@gmail.com>
2024-07-06 10:54:34 -0300

    Create User and Task repositories

---
+++ src/main/java/devJucelio/com/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package devJucelio.com.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import devJucelio.com.todosimple.models.Task;
+import devJucelio.com.todosimple.models.projection.TaskProjection;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<TaskProjection> findByUser_Id(Long id);
+
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery =
+    // true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+}
