ae49b3200d8594baad5f91b49e6e56878a6f080a
JUCELIO <juceliocoelho2022@gmail.com>
2024-07-06 14:18:02 -0300

    Create UserController

---
+++ src/main/java/devJucelio/com/todosimple/controllers/UserController.java
@@ -0,0 +1,53 @@
+package devJucelio.com.todosimple.controllers;
+
+import devJucelio.com.todosimple.models.User;
+import devJucelio.com.todosimple.models.dto.UserCreateDTO;
+import devJucelio.com.todosimple.models.dto.UserUpdateDTO;
+import devJucelio.com.todosimple.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = this.userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody UserCreateDTO obj) {
+        User user = this.userService.fromDTO(obj);
+        User newUser = this.userService.create(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody UserUpdateDTO obj, @PathVariable Long id) {
+        obj.setId(id);
+        User user = this.userService.fromDTO(obj);
+        this.userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
